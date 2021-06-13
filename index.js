940ba1a7fc64f37a01fb4cb0c328e75651eaf6ea
github username <github email address>
2021-06-13 13:55:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code here!
-document.querySelector('main').remove();
+document.querySelector('main').remove()
 
-let newHeader = document.createElement('h1');
+const newHeader = document.createElement('h1')
 
-document.body.appendChild(newHeader);
+document.body.appendChild(newHeader)
 
-newHeader.id = 'victory';
+newHeader.id = 'victory'
 
-newHeader.innerHTML = '<h1>victory</h1> <p>YOUR-NAME is the champion</p>';
+newHeader.innerHTML = '<h1>victory</h1> <p>YOUR-NAME is the champion</p>'
